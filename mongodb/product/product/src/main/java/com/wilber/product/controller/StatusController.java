1d7e37311abfd39d10926c68a360702a7aeb9655
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-14 22:58:34 -0300

    mudanca swagger

--- mongodb/product/product/src/main/java/com/wilber/product/controller/StatusController.java
+++ mongodb/product/product/src/main/java/com/wilber/product/controller/StatusController.java
@@ -1,7 +1,11 @@
 package com.wilber.product.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/status")
-@Api(value = "Status endpoint",tags = {"Produtos end"})
 @CrossOrigin(origins = "*")
 public class StatusController {
 
     @GetMapping("/get")
-    @ApiOperation(value = "Retorna o status")
+    @Operation(summary = "Retorna o status")
+    @ApiResponse(responseCode = "200", description = "Retorna Status do Aplicativo")
     public String getStatus(){
         return "online";
     }
